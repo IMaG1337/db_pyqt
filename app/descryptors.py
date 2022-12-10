5dd81d7ce77a22f0313fa80f0df6dd16a5853fa8
IMaG1337 <007mail.ru96@mail.ru>
2022-12-10 12:44:55 +0300

    lesson_5

--- app/descryptors.py
+++ app/descryptors.py
@@ -1,12 +1,22 @@
 import logging
-logger = logging.getLogger('server')
+import sys
+
+# Инициализиция логера
+# метод определения модуля, источника запуска.
+if sys.argv[0].find('client') == -1:
+    # если не клиент то сервер!
+    logger = logging.getLogger('server')
+else:
+    # ну, раз не сервер, то клиент
+    logger = logging.getLogger('client')
 
 
+# Дескриптор для описания порта:
 class Port:
     def __set__(self, instance, value):
         if not 1023 < value < 65536:
             logger.critical(
-                f'Попытка запуска сервера с указанием неподходящего порта {value}. Допустимы адреса с 1024 до 65535.')
+                f'Попытка запуска с указанием неподходящего порта {value}. Допустимы адреса с 1024 до 65535.')
             exit(1)
         instance.__dict__[self.name] = value
 
